b387df9522be4a65275ea81c51325b4478be59aa
jvanderen1 <livetoscout@gmail.com>
2018-09-03 02:08:51 -0700

    Updated deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@
 set -e
 
 # build
-npm run build
+npm run build:modern
 
 # navigate into the build output directory
 cd dist
@@ -17,9 +17,9 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:jvanderen1/jvanderen1.github.io.git master
+#git push -f git@github.com:jvanderen1/jvanderen1.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:jvanderen1/personal-website.git master:gh-pages
+git push -f git@github.com:jvanderen1/personal-website.git master:gh-pages
 
 cd -
